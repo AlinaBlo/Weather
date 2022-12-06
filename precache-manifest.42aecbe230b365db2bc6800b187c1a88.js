8c89b3a32e600894d69c10da98d1dd9916ab70c0
AlinaBlo <82020749+AlinaBlo@users.noreply.github.com>
2022-12-06 23:17:04 +0200

    Updates

---
+++ precache-manifest.42aecbe230b365db2bc6800b187c1a88.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "aba760dd87033592c8c0",
+    "url": "/weather/static/css/main.cdbafbbc.chunk.css"
+  },
+  {
+    "revision": "aba760dd87033592c8c0",
+    "url": "/weather/static/js/main.aba760dd.chunk.js"
+  },
+  {
+    "revision": "1743278c051df87abac6",
+    "url": "/weather/static/js/1.1743278c.chunk.js"
+  },
+  {
+    "revision": "ea5c9e2db16b76d05cb5",
+    "url": "/weather/static/css/2.9eb0cc49.chunk.css"
+  },
+  {
+    "revision": "ea5c9e2db16b76d05cb5",
+    "url": "/weather/static/js/2.ea5c9e2d.chunk.js"
+  },
+  {
+    "revision": "a9b637ec008a163a6f82",
+    "url": "/weather/static/js/runtime~main.a9b637ec.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/weather/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/weather/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/weather/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/weather/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/weather/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/weather/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/weather/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/weather/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/weather/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/weather/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/weather/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/weather/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/weather/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/weather/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/weather/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/weather/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "15b588ef9ffe7b1ec92ed038cdcd8164",
+    "url": "/weather/index.html"
+  }
+];
