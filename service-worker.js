cf4304ab264032b02cf332ad1e39ca43b547b3a3
AlinaBlo <82020749+AlinaBlo@users.noreply.github.com>
2022-12-13 00:09:31 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather/precache-manifest.42aecbe230b365db2bc6800b187c1a88.js"
+  "/weather/precache-manifest.f11225d36cd76142932e7aed34dc7597.js"
 );
 
 workbox.clientsClaim();
